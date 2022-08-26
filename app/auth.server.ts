9757b1724fbb7c72a3a84fe7cffb75a01444248b
Muhammad Taufiq Febrianto <taufiqisfebrianto06@gmail.com>
2022-08-26 17:20:41 +0700

    Tambah APP_URL di env

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -25,12 +25,14 @@ export const authenticator = new Authenticator<string | null>(sessionStorage, {
 	sessionKey: 'userId'
 });
 
+const appURL = process.env.APP_URL ?? `http://localhost:3000`;
+
 authenticator.use(
 	new GoogleStrategy(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: `http://localhost:3000/auth/${SocialsProvider.GOOGLE}/callback`
+			callbackURL: `${appURL}/auth/${SocialsProvider.GOOGLE}/callback`
 		},
 		async params => {
 			const email = params.profile._json.email;
